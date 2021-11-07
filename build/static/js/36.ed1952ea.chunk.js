a3f85f558bce3f66224727161eb1f4490631caf1
Nghia <nghiait06@gmail.com>
2021-11-07 15:45:03 +0000

    update

---
+++ build/static/js/36.ed1952ea.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[36],{1028:function(e,t,r){"use strict";r.r(t);var c=r(165),a=r(166),n=r(168),i=r(167),u=r(2),o=r(17),s=function(e){Object(n.a)(r,e);var t=Object(i.a)(r);function r(e){var a;return Object(c.a)(this,r),(a=t.call(this,e)).state={},a}return Object(a.a)(r,[{key:"render",value:function(){return Object(o.jsx)("div",{children:"COMMING SOON"})}}]),r}(u.Component);t.default=s}}]);
+//# sourceMappingURL=36.ed1952ea.chunk.js.map
