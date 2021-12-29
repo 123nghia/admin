f6061f2c91f04fc475928eb333a058a0ca14450f
Nghia <nghiait06>
2021-12-29 14:00:21 +0700

    init dev

--- src/contants/contants.js
+++ src/contants/contants.js
@@ -1,9 +1,9 @@
 export default {
   // "BASE_URL" : "https://api-soida.applamdep.com",
-  "BASE_URL" : "http://localhost:3002",
+  "BASE_URL" : "https://api-pensilia.applamdep.com",
   // "BASE_URL_CURRENT" : "https://id.applamdep.com/",
-  "BASE_URL_HISTORY_SKIN" : "https://applamdep.com/soida/xemchitietlichsu/",
-  "BASE_URL_CURRENT" : "http://localhost:3003",
+  "BASE_URL_HISTORY_SKIN" : "https://pensilia.applamdep.com/soida/xemchitietlichsu/",
+  "BASE_URL_CURRENT" : "https://pensilia-admin.applamdep.com",
   "LIST_USER" : "/api/list-user",
   "LIST_USER_WITH_ID" : "/api/list-user?id=",
   "ADD_USER" : "/api/add-user",
