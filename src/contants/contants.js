96762d8a10bd88d563590413c06f8b979afe2584
Nghia <nghiait06>
2022-01-05 20:36:10 +0700

    update api

--- src/contants/contants.js
+++ src/contants/contants.js
@@ -1,5 +1,5 @@
 export default {
-  "BASE_URL" : "https://api-soida.applamdep.com",
+  "BASE_URL" : "https://api-pensilia.applamdep.com/",
   //  "BASE_URL" : "http://localhost:3012",
   "BASE_URL_CURRENT" : "https://pensilia-admin.applamdep.com/",
   "BASE_URL_HISTORY_SKIN" : "https://pensilia.applamdep.com/soida/xemchitietlichsu/",
