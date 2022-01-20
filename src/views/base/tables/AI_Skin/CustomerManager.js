f322c9b0cabeb4094c17e717b26e439d016825e4
Nghia <nghiait06>
2022-01-20 09:54:24 +0700

    fix code

--- src/views/base/tables/AI_Skin/CustomerManager.js
+++ src/views/base/tables/AI_Skin/CustomerManager.js
@@ -482,7 +482,7 @@ render() {
                     </tr>
                   </thead>
                   <tbody>
-                    <td colSpan="10" hidden={data.length > 0 ? true : false} className="text-center">Không tìm thấy dữ liệu</td>
+                    <td colSpan="10" hidden={( data != null ) ? true : false} className="text-center">Không tìm thấy dữ liệu</td>
                     {
                       dataModalHistory != undefined ?
                       dataModalHistory.map((item, i) => {
