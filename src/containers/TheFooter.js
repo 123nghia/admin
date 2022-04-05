7420920d4edea61e0416d245e62219151b6530fd
Nguy?n Th? H?ng Quyên <hongquyen@gmail.com>
2022-04-05 12:00:56 +0700

    ++add copyright footer

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { CFooter } from '@coreui/react'
+import React from "react";
+import { CFooter } from "@coreui/react";
+
+const getDateNow = () => {
+  return new Date().getFullYear();
+};
 
 const TheFooter = () => {
   return (
-    <CFooter fixed={false}>
-
+    <CFooter fixed={false} style={{ justifyContent: "center", gap: "0.3rem" }}>
+      &copy; {getDateNow()}{" "}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://deal24h.vn/"
+        style={{ color: "rgb(70, 128, 255)", fontWeight: "bold" }}
+      >
+        TIKITECH
+      </a>{" "}
+      COPYRIGHT. ALL RIGHTS RESERVED.
     </CFooter>
-  )
-}
+  );
+};
 
-export default React.memo(TheFooter)
+export default React.memo(TheFooter);
