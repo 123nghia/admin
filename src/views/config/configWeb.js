c35c81701f55d5b33918e1a115f262464544bf2f
Phongdz <hothanhphong2611@gmail.com>
2022-05-27 17:13:35 +0700

    logo

--- src/views/config/configWeb.js
+++ src/views/config/configWeb.js
@@ -42,6 +42,10 @@ import ChangeColor from "./configWeb/Color";
 import Homepage from "./configWeb/Homepage";
 import VoucherPartner from "./configWeb/VoucherPartner";
 import { BiSlideshow } from 'react-icons/bi';
+import { MdOutlinePermDataSetting } from 'react-icons/md';
+import { BsChatDots } from 'react-icons/bs';
+import { IoLogoInstagram ,IoColorPaletteOutline} from 'react-icons/io5';
+
 
 let headers = new Headers();
 const auth = localStorage.getItem("auth");
@@ -68,25 +72,14 @@ class ConfigWeb extends Component {
           _id: "3",
           name: "Cấu hình SEO",
           icon: (
-            <PermDataSettingIcon style={{ width: "24px ", height: "24px " }} />
+            <MdOutlinePermDataSetting style={{ width: "24px ", height: "24px " }} />
           ),
         },
         {
           _id: "9",
           name: "Banner",
           icon: (
-            <svg
-              style={{ width: "24px ", height: "24px " }}
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-file-slides-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M7 7.78V5.22c0-.096.106-.156.19-.106l2.13 1.279a.125.125 0 0 1 0 .214l-2.13 1.28A.125.125 0 0 1 7 7.778z" />
-              <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 .496.438l.5 4A.5.5 0 0 1 11.5 9h-3v2.016c.863.055 1.5.251 1.5.484 0 .276-.895.5-2 .5s-2-.224-2-.5c0-.233.637-.429 1.5-.484V9h-3a.5.5 0 0 1-.496-.562l.5-4A.5.5 0 0 1 5 4z" />
-            </svg>
+            <BiSlideshow style={{ width: "24px ", height: "24px " }} />
           ),
         },
         {
@@ -125,19 +118,19 @@ class ConfigWeb extends Component {
         {
           _id: "5",
           name: "Mã Chat",
-          icon: <AiFillWechat style={{ width: "24px ", height: "24px " }} />,
+          icon: <BsChatDots style={{ width: "24px ", height: "24px " }} />,
         },
         {
           _id: "6",
           name: "Cấu hình mạng xã hội",
-          icon: <FacebookIcon style={{ width: "24px ", height: "24px " }} />,
+          icon: <IoLogoInstagram style={{ width: "24px ", height: "24px " }} />,
         },
     
         {
           _id: "8",
           name: "Quản lý màu sắc",
           icon: (
-            <IoIosColorPalette style={{ width: "24px ", height: "24px " }} />
+            <IoColorPaletteOutline style={{ width: "24px ", height: "24px " }} />
           ),
         },
       ],
